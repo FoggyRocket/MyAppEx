7e7d398bb7ca125b30afa377b1ed8d7c8457bae7
salemm <emmanuel@bquate.com>
2019-04-11 20:22:23 -0500

    merge con cambios de dev :skull:

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+<<<<<<< Updated upstream
 console.log('hay wey ');
+=======
+//comentario de kain
+/**
+ * @function [mates] esta funcion hace mates
+ */
+console.log('hay wey ');
+//app example
+
+getName=(name)=>{
+    console.log('this part is write by:',name)
+}
+getName('Dylan')
+
+
+const fizz = ()=> {
+    for(let i=0; i < 100; i++){
+        if(i %15 == 0) console.log('fizzbuzz');
+         else if(i % 3 == 0) console.log('fizz');  
+         else if(i % 5 == 0)console.log('buzz');
+         else console.log(i)
+    }
+}
+
+fizz()
+>>>>>>> Stashed changes
